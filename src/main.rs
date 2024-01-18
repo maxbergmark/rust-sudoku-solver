5efbd40c50a9594077c05e10fab0912a789a2631
Max Bergmark <max.bergmark@gmail.com>
2024-01-18 16:44:30 +0100

    Functional refactor + optimization

--- src/main.rs
+++ src/main.rs
@@ -6,45 +6,44 @@ pub mod debug;
 
 #[allow(unused)]
 use rayon::prelude::*;
-use std::{fs::read_to_string, fmt::Write, time::Instant};
-use sudoku::Sudoku;
+use std::{fs::read_to_string, fmt::Write, time::Instant, str::FromStr};
+use sudoku::{Sudoku, SudokuError};
 
 
-fn read_lines(filename: &str) -> Vec<Sudoku> {
-    read_to_string(filename).unwrap()
+fn read_lines(filename: &str) -> Result<Vec<Sudoku>, SudokuError> {
+    read_to_string(filename)
+        .map_err(|_| SudokuError::ParseError)?
         .lines()
         .skip(1)
-        .map(|line| {
-            Sudoku::new(line)
-        }).collect()
+        .map(Sudoku::from_str)
+        .collect()
 }
 
-fn solve_all_in_file(filename: &str) -> Vec<Sudoku> {
-    let sudokus = read_lines(filename);
+fn solve_all_in_file(filename: &str) -> Result<Vec<Sudoku>, SudokuError> {
+    let sudokus = read_lines(filename)?;
     sudokus
         .into_iter()
         // .into_par_iter()
-        .enumerate()
-        .map(|(_i, s)| {
-            s.solve().unwrap()
-        }).collect()
+        .map(sudoku::solve)
+        .collect()
 }
 
 fn benchmark_file(filename: &str) {
     let now = Instant::now();
-    let solutions = solve_all_in_file(filename);
+    let solutions = solve_all_in_file(filename).unwrap();
     let elapsed = now.elapsed();
     let n = solutions.len();
     let time_per_puzzle = elapsed / u32::try_from(n).unwrap();
 
-    let total_recursions: i32 = solutions.iter().map(|&s| s.num_recursions).sum();
-    let max_recursions: i32 = solutions.iter().map(|&s| s.num_recursions).max().unwrap();
+    let total_recursions: i32 = solutions.iter().map(|s| s.num_recursions).sum();
+    let max_recursions: i32 = solutions.iter().map(|s| s.num_recursions).max().unwrap();
     let recursions_per_puzzle = total_recursions as f32 / n as f32;
 
-    let total_guesses: i32 = solutions.iter().map(|&s| s.guesses).sum();
+    let total_guesses: i32 = solutions.iter().map(|s| s.guesses).sum();
     let avg_guesses = total_guesses as f32 / n as f32;
 
-    println!("\r{filename:45}{n:>12}    {elapsed:10.2?}{time_per_puzzle:12.2?}{recursions_per_puzzle:15.4}{max_recursions:15}{avg_guesses:12.4}");
+    print!("\r{filename:45}{n:>12}    {elapsed:10.2?}{time_per_puzzle:12.2?}");
+    println!("{recursions_per_puzzle:15.4}{max_recursions:15}{avg_guesses:12.4}");
 }
 fn validate(s: &Sudoku, solution: &Sudoku) -> bool {
     for (&d1, d2) in s.digits.iter().zip(solution.digits) {
@@ -56,26 +55,26 @@ fn validate(s: &Sudoku, solution: &Sudoku) -> bool {
 }
 
 #[allow(unused)]
-fn output_solutions(filename: &str) -> String {
-    let sudokus = read_lines(filename);
+fn output_solutions(filename: &str) -> Result<String, SudokuError> {
+    let sudokus = read_lines(filename)?;
     let mut ret = String::new();
     writeln!(ret, "{}", sudokus.len());
     sudokus.iter().enumerate().for_each(|(i, s)| {
-        let solution = s.solve().unwrap();
+        let solution = sudoku::solve(s.clone()).unwrap_or(s.clone());
         assert!(validate(s, &solution));
         writeln!(ret, "{s},{solution}");
     });
     print!("{ret}");
-    ret
+    Ok(ret)
 }
 
 #[allow(unused)]
 fn benchmark() {
-    println!("{:45}{:>12}    {:10}{:12}{:>15}{:>15}{:>12}", "filename", "num sudokus", "time", "per puzzle", "avg_recursions", "max_recursions", "avg_guesses");
+    println!("{:45}{:>12}    {:10}{:12}{:>15}{:>15}{:>12}",
+        "filename", "num sudokus", "time", "per puzzle", "avg_recursions", "max_recursions", "avg_guesses");
     benchmark_file("data-sets/easiest.txt");
     benchmark_file("data-sets/hard_sudokus.txt");
     benchmark_file("data-sets/all_17_clue_sudokus.txt");
-    // benchmark_file("data-sets/hardest.txt");
     benchmark_file("data-sets/puzzles6_forum_hardest_1106.txt");
     benchmark_file("data-sets/ph1307.txt");
     // benchmark_file("data-sets/ph1910_01.txt");
@@ -97,10 +96,9 @@ mod tests {
     use md5;
 
     fn parse_and_expect(filename: &str, expected_digest: &str) {
-        let output = output_solutions(filename);
+        let output = output_solutions(filename).unwrap();
         let digest = md5::compute(output.as_bytes());
         assert_eq!(format!("{:x}", digest), expected_digest);
-
     }
 
     #[test]
@@ -125,5 +123,19 @@ mod tests {
             "data-sets/all_17_clue_sudokus.txt", 
             "41704fd7d8fd0723a45ffbb2dbbfa488"
         );
+    }
+
+    fn count_recursions(filename: &str, expected: i32) {
+        let solutions = solve_all_in_file(filename).unwrap();
+        let recursions = solutions.iter().map(|s| s.num_recursions).sum::<i32>();
+        assert_eq!(expected, recursions);
+    }
+
+    #[test]
+    fn test_17_recursions() {
+        count_recursions("data-sets/all_17_clue_sudokus.txt", 262592);
+        count_recursions("data-sets/easiest.txt", 0);
+        count_recursions("data-sets/hard_sudokus.txt", 15240);
+        count_recursions("data-sets/puzzles6_forum_hardest_1106.txt", 83611);
     }
 }
--- src/visible.rs
+++ src/visible.rs
@@ -1,111 +1,85 @@
 use crate::{consts, sudoku::{Sudoku, SudokuError}};
 
 
-
-impl Sudoku {
-
-    #[allow(unused)]
-    pub(crate) fn check_all_visible_singles(&mut self) -> Result<(), SudokuError> {
-        // loop {
-            let mut placements = vec![];
-            for (i, &n) in self.bitboard.iter().enumerate() {
-                if n.count_ones() == 1 {
-                    placements.push((i, n.trailing_zeros() as consts::BitWidth));
-                }
-            }
-
-            // if placements.is_empty() {
-                // break;
-            // } else {
-                for (i, n) in placements {
-                    // println!("from visible singles");
-                    if self.digits[i] == 0 {
-                        *self = self.place_and_copy(i, n)?;
-                    }
-                }    
-            // }
-        // }
-        Ok(())
-    }
-
-    #[inline]
-    fn check_visible_double_pair(&mut self, idx: usize, neighbor: usize, neighbors: &[usize; 8]) -> Result<(), SudokuError> {
-        if self.bitboard[idx] == self.bitboard[neighbor] {
-            let mask = consts::MASK ^ self.bitboard[idx];
-            for n_idx in neighbors {
-                if *n_idx != neighbor {
-                    self.bitboard[*n_idx] &= mask;
-                    if (self.digits[*n_idx] == 0) & (self.bitboard[*n_idx] == 0) {
-                        return Err(SudokuError {
-                            digit: 0,
-                            index: idx,
-                            num_recursions: self.num_recursions, 
-                            guesses: self.guesses
-                        });
-                    }
-                    if (self.digits[*n_idx] == 0) & (self.bitboard[*n_idx].count_ones() == 1) {
-                        // println!("placing immediately from double pair");
-                        let digit = self.bitboard[*n_idx].trailing_zeros() as consts::BitWidth;
-                        *self = self.place_and_copy(*n_idx, digit)?;
-                    } 
-                }
-            }
-        }
-        Ok(())
-    }
-
-    fn check_visible_doubles(&mut self, neighbor_arr: &[[usize; 8]; consts::SIZE]) -> Result<(), SudokuError> {
-        for (idx, neighbors) in neighbor_arr.iter().enumerate() {
-            if (self.bitboard[idx] > 0) & (self.bitboard[idx].count_ones() == 2) {
-                for neighbor in neighbors {
-                    self.check_visible_double_pair(idx, *neighbor, neighbors)?;
-                }
-            }
-        }
-        Ok(())
-    }
-
-    pub(crate) fn check_all_visible_doubles(&mut self) -> Result<(), SudokuError> {
-        self.check_visible_doubles(&consts::SAME_ROW)?;
-        self.check_visible_doubles(&consts::SAME_COL)?;
-        self.check_visible_doubles(&consts::SAME_CELL)
-    }
-
-    fn check_visible_n(&mut self, n: usize, neighbor_arr: &[usize; consts::WIDTH]) -> Result<(), SudokuError> {
-
-        use itertools::Itertools;
-        for idxs in neighbor_arr.iter().filter(|&i| self.digits[*i] == 0).combinations(n) {
-            let mut mask = 0;
-            for &i in idxs.iter() {
-                mask |= self.bitboard[*i];
-            }
-            if mask.count_ones() == n as u32 {
-                let inverted_mask = consts::MASK ^ mask;
-                for idx in neighbor_arr {
-                    if !idxs.contains(&idx) {
-                        self.bitboard[*idx] &= inverted_mask;
-                        if (self.bitboard[*idx] == 0) & (self.digits[*idx] == 0) {
-                            return Err(SudokuError {
-                                digit: 0,
-                                index: *idx,
-                                num_recursions: self.num_recursions,
-                                guesses: self.guesses
-                            });
-                        }
-                    }
-                }
-            }
-        }
-        Ok(())
-    }
-
-    #[allow(unused)]
-    pub(crate) fn check_all_visible_n(&mut self, n: usize) -> Result<(), SudokuError> {
-        for i in 0..9 {
-            self.check_visible_n(n, &consts::ROWS[i])?;
-            self.check_visible_n(n, &consts::COLS[i])?;
-            self.check_visible_n(n, &consts::CELLS[i])?;
-        }
-        Ok(())
-    }
+fn get_placements(sudoku: &Sudoku) -> Vec<(usize, consts::BitWidth)> {
+    sudoku.bitboard.iter().enumerate()
+        .filter(|(_, &n)| n.count_ones() == 1)
+        .map(|(i, n)| (i, n.trailing_zeros() as consts::BitWidth))
+        .collect()
+        
 }
+
+pub(crate) fn place_all_visible_singles(sudoku: &mut Sudoku) -> Result<(), SudokuError> {
+    for (i, n) in get_placements(sudoku) {
+        if sudoku.digits[i] == 0 {
+            sudoku.place_and_propagate(i, n)?;
+        }
+    }
+    Ok(())
+}
+
+fn check_visible_double_possible(sudoku: &Sudoku, n_idx: usize) -> Result<(), SudokuError> {
+    if (sudoku.digits[n_idx] == 0) && (sudoku.bitboard[n_idx] == 0) {
+        Err(SudokuError::from(sudoku))
+    } else {
+        Ok(())
+    }
+
+}
+
+fn place_visible_double(sudoku: &mut Sudoku, n_idx: usize, mask: consts::BitWidth) -> Result<(), SudokuError> {
+    sudoku.bitboard[n_idx] &= mask;
+    check_visible_double_possible(sudoku, n_idx)?;
+    if sudoku.bitboard[n_idx].count_ones() == 1 {
+        let digit = sudoku.bitboard[n_idx].trailing_zeros() as consts::BitWidth;
+        sudoku.place_and_propagate(n_idx, digit)?;
+    } 
+    Ok(())
+}
+
+fn check_visible_double_pair(sudoku: &mut Sudoku, idx: usize, neighbor: usize, neighbors: &[usize; 8]) -> Result<(), SudokuError> {
+    let mask = consts::MASK ^ sudoku.bitboard[idx];
+    for &n_idx in neighbors {
+        if n_idx != neighbor {
+            place_visible_double(sudoku, n_idx, mask)?;
+        }
+    }
+    Ok(())
+}
+
+fn place_visible_double_pair(sudoku: &mut Sudoku, idx: usize, neighbors: &[usize; 8]) -> Result<(), SudokuError> {
+    for neighbor in neighbors {
+        if sudoku.bitboard[idx] == sudoku.bitboard[*neighbor] {
+            check_visible_double_pair(sudoku, idx, *neighbor, neighbors)?;
+        }
+    }
+    Ok(())
+}
+
+fn place_visible_doubles(sudoku: &mut Sudoku, neighbor_arr: &[[usize; 8]; consts::SIZE]) -> Result<(), SudokuError> {
+    for (idx, neighbors) in neighbor_arr.iter().enumerate() {
+        if sudoku.bitboard[idx].count_ones() == 2 {
+            place_visible_double_pair(sudoku, idx, neighbors)?;
+        }
+    }
+    Ok(())
+}
+
+fn place_visible_doubles_rows(sudoku: &mut Sudoku) -> Result<(), SudokuError> {
+    place_visible_doubles(sudoku, &consts::SAME_ROW)
+}
+
+fn place_visible_doubles_cols(sudoku: &mut Sudoku) -> Result<(), SudokuError> {
+    place_visible_doubles(sudoku, &consts::SAME_COL)
+}
+
+fn place_visible_doubles_cells(sudoku: &mut Sudoku) -> Result<(), SudokuError> {
+    place_visible_doubles(sudoku, &consts::SAME_CELL)
+}
+
+pub(crate) fn place_all_visible_doubles(sudoku: &mut Sudoku) -> Result<(), SudokuError> {
+    place_visible_doubles_rows(sudoku)?;
+    place_visible_doubles_cols(sudoku)?;
+    place_visible_doubles_cells(sudoku)
+}
+
